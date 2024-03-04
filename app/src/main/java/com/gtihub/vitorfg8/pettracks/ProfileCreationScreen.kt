b5da574362670b8f0a2eced461ce0d6fbcb9efd4
Vítor Ferreira Guimarães <vitorfer237@gmail.com>
2024-03-04 16:32:14 -0300

    add date picker

--- app/src/main/java/com/gtihub/vitorfg8/pettracks/ProfileCreationScreen.kt
+++ app/src/main/java/com/gtihub/vitorfg8/pettracks/ProfileCreationScreen.kt
@@ -2,19 +2,13 @@ package com.gtihub.vitorfg8.pettracks
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -22,16 +16,13 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -49,41 +40,26 @@ import com.gtihub.vitorfg8.pettracks.ui.theme.PetTracksTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileCreationScreen(
-    onBackPressed: () -> Unit = {},
-    onAddPressed: () -> Unit = {}
+    onBackPressed: () -> Unit = {}, onAddPressed: () -> Unit = {}
 ) {
     Scaffold(
         topBar = {
-            TopAppBar(
-                colors = topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text(text = "Add Pet") // TODO
-                },
-                navigationIcon = {
-                    IconButton(
-                        modifier = Modifier.size(20.dp),
-                        onClick = { onBackPressed() }) {
-                        Icon(
-                            painterResource(id = R.drawable.arrow_left_solid),
-                            contentDescription = "",
-                            tint = MaterialTheme.colorScheme.surfaceTint
-                        )
-                    }
+            TopAppBar(colors = topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary,
+            ), title = {
+                Text(text = "Add Pet") // TODO
+            }, navigationIcon = {
+                IconButton(modifier = Modifier.size(20.dp), onClick = { onBackPressed() }) {
+                    Icon(
+                        painterResource(id = R.drawable.arrow_left_solid),
+                        contentDescription = "",
+                        tint = MaterialTheme.colorScheme.surfaceTint
+                    )
                 }
-            )
+            })
         },
     ) { innerPadding ->
-
-        var showSheet by remember { mutableStateOf(false) }
-        if (showSheet) {
-            PictureBottomSheet {
-                showSheet = false
-            }
-        }
-
         Column(
             modifier = Modifier
                 .padding(innerPadding)
@@ -92,58 +68,20 @@ fun ProfileCreationScreen(
         ) {
             ProfilePictureUpdater {}
             TypeSelector()
-            var name by remember { mutableStateOf("") }
             TextField(
-                modifier = Modifier.padding(8.dp),
-                value = name,
-                onValueChange = { name = it },
-                label = { Text(stringResource(R.string.name)) },
+                label = stringResource(R.string.name),
                 keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Words)
             )
-
-            var gender by remember { mutableStateOf("") }
+            TextField(label = stringResource(R.string.gender))
             TextField(
-                modifier = Modifier.padding(8.dp),
-                value = gender,
-                onValueChange = { gender = it },
-                label = { Text(stringResource(R.string.gender)) }
-            )
-
-            var breed by remember {
-                mutableStateOf("")
-            }
-            TextField(
-                modifier = Modifier.padding(8.dp),
-                value = breed,
-                onValueChange = { breed = it },
-                label = { Text(stringResource(R.string.breed)) },
+                label = stringResource(R.string.breed),
                 keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Words)
             )
-            var birthDate by remember {
-                mutableStateOf("")
-            }
-
+            TextInputDatePicker()
             TextField(
-                modifier = Modifier.padding(8.dp),
-                value = birthDate,
-                onValueChange = { birthDate = it },
-                label = { Text(stringResource(R.string.birth_date)) },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-
-            )
-
-            var weight by remember {
-                mutableFloatStateOf(0.0f)
-            }
-
-            TextField(
-                modifier = Modifier.padding(8.dp),
-                value = weight.toString(),
-                onValueChange = { weight = it.toFloat() },
-                label = { Text(stringResource(R.string.weight)) },
+                label = stringResource(R.string.weight),
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
             )
-
             Button(onClick = { onAddPressed() }) {
                 Text(text = "Add")
             }
@@ -151,35 +89,20 @@ fun ProfileCreationScreen(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PictureBottomSheet(onDismiss: () -> Unit = {}) {
-    val modalBottomSheetState = rememberModalBottomSheetState()
-
-    ModalBottomSheet(
-        onDismissRequest = { onDismiss() },
-        sheetState = modalBottomSheetState,
-        dragHandle = { BottomSheetDefaults.DragHandle() },
-    ) {
-        Column {
-            BottomSheetItem(icon = painterResource(id = R.drawable.camera_solid), "Camera") {
-                openCamera()
-            }
-            BottomSheetItem(icon = painterResource(id = R.drawable.file_image_solid), "Gallery") {
-                openGallery()
-            }
-        }
-    }
+private fun TextField(
+    label: String,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default
+) {
+    var name by remember { mutableStateOf("") }
+    TextField(
+        modifier = Modifier.padding(8.dp),
+        value = name,
+        onValueChange = { name = it },
+        label = { Text(label) },
+        keyboardOptions = keyboardOptions
+    )
 }
-
-fun openGallery() {
-
-}
-
-fun openCamera() {
-
-}
-
 
 @Composable
 fun TypeSelector() {
@@ -240,45 +163,11 @@ fun TypeItem(name: String, painter: Painter, isSelected: Boolean = false, onClic
     }
 }
 
-@Composable
-fun BottomSheetItem(
-    icon: Painter,
-    text: String = "",
-    onClick: () -> Unit
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(40.dp)
-            .clickable { onClick() }
-            .padding(8.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(
-            painter = icon,
-            contentDescription = null,
-            modifier = Modifier.size(24.dp)
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        Text(
-            text = text,
-        )
-    }
-}
-
 @Preview
 @Composable
 fun ProfileCreationPreview() {
     PetTracksTheme {
         ProfileCreationScreen()
-    }
-}
-
-@Preview(showBackground = true)
-@Composable()
-fun PictureBottomSheetPreview() {
-    PetTracksTheme {
-        BottomSheetItem(painterResource(id = R.drawable.camera_solid), "Camera") {}
     }
 }
 
---
+++ app/src/main/java/com/gtihub/vitorfg8/pettracks/TextInputDatePicker.kt
@@ -0,0 +1,77 @@
+package com.gtihub.vitorfg8.pettracks
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TextInputDatePicker() {
+    val focusManager = LocalFocusManager.current
+    var showDatePickerDialog by remember {
+        mutableStateOf(false)
+    }
+    val datePickerState = rememberDatePickerState()
+    val selectedDate = datePickerState.selectedDateMillis?.toLocalDateFormat() ?: ""
+
+    if (showDatePickerDialog) {
+        DatePickerDialog(
+            onDismissRequest = { showDatePickerDialog = false },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        showDatePickerDialog = false
+                    }) {
+                    Text(text = "Escolher data")
+                }
+            }) {
+            DatePicker(state = datePickerState)
+        }
+    }
+    TextField(
+        value = selectedDate,
+        onValueChange = { },
+        Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .onFocusEvent {
+                if (it.isFocused) {
+                    showDatePickerDialog = true
+                    focusManager.clearFocus(force = true)
+                }
+            },
+        label = {
+            Text("Date")
+        },
+        readOnly = true
+    )
+}
+
+fun Long.toLocalDateFormat(): String {
+    val date = Date(this)
+    val dateFormat = SimpleDateFormat.getDateInstance(SimpleDateFormat.SHORT, Locale.getDefault())
+    val pattern = (dateFormat as SimpleDateFormat).toPattern()
+    val formatter = SimpleDateFormat(pattern, Locale.getDefault()).apply {
+        timeZone = TimeZone.getTimeZone("GMT")
+    }
+    return formatter.format(date)
+}
