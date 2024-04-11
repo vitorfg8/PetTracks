11aff2b997d7073680cc196b27852e5a82d430e5
Vítor Ferreira Guimarães <vitorfer237@gmail.com>
2024-04-11 16:16:40 -0300

    refactoring

--- app/src/main/java/com/gtihub/vitorfg8/pettracks/ui/ProfileCreationScreen.kt
+++ app/src/main/java/com/gtihub/vitorfg8/pettracks/ui/ProfileCreationScreen.kt
@@ -1,5 +1,7 @@
 package com.gtihub.vitorfg8.pettracks.ui
 
+import android.content.Context
+import android.net.Uri
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -32,7 +34,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -41,26 +42,33 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gtihub.vitorfg8.pettracks.R
+import com.gtihub.vitorfg8.pettracks.ui.model.PetDataUi
 import com.gtihub.vitorfg8.pettracks.ui.model.PetTypeDataUi
 import com.gtihub.vitorfg8.pettracks.ui.theme.PetTracksTheme
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStream
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileCreationScreen(
     onBackPressed: () -> Unit = {}, onAddPressed: () -> Unit = {}
 ) {
+
+    val pet by remember { mutableStateOf(PetDataUi()) }
+
     Scaffold(
         topBar = {
             TopAppBar(colors = topAppBarColors(
                 containerColor = MaterialTheme.colorScheme.primaryContainer,
                 titleContentColor = MaterialTheme.colorScheme.primary,
             ), title = {
-                Text(text = "Add Pet") // TODO
+                Text(text = stringResource(R.string.add_pet))
             }, navigationIcon = {
                 IconButton(modifier = Modifier.size(20.dp), onClick = { onBackPressed() }) {
                     Icon(
                         painterResource(id = R.drawable.arrow_left_solid),
-                        contentDescription = "",
+                        contentDescription = "", //TODO
                         tint = MaterialTheme.colorScheme.surfaceTint
                     )
                 }
@@ -75,7 +83,9 @@ fun ProfileCreationScreen(
         ) {
             ProfilePictureUpdater(
                 modifier = Modifier.padding(vertical = 16.dp, horizontal = 32.dp)
-            ) {}
+            ) {
+                //pet.profilePicture = it.toByteArray(LocalContext.current)
+            }
             TypeSelector()
             TextField(
                 label = stringResource(R.string.name),
@@ -98,7 +108,7 @@ fun ProfileCreationScreen(
                 modifier = Modifier
                     .padding(vertical = 8.dp, horizontal = 32.dp)
                     .fillMaxWidth(),
-                suffix = { Text(text = "Kg") },
+                suffix = { Text(text = stringResource(R.string.kg)) },
                 value = weight.orEmpty(),
                 onValueChange = { weight = it },
                 label = { Text(stringResource(id = R.string.weight)) },
@@ -115,10 +125,41 @@ fun ProfileCreationScreen(
     }
 }
 
+private fun Uri?.toByteArray(context: Context): ByteArray? {
+    if (this == null) {
+        return null
+    }
+
+    var inputStream: InputStream? = null
+    try {
+        inputStream = context.contentResolver.openInputStream(this)
+        if (inputStream != null) {
+            val byteBuffer = ByteArrayOutputStream()
+            val bufferSize = 1024
+            val buffer = ByteArray(bufferSize)
+            var len: Int
+            while (inputStream.read(buffer).also { len = it } != -1) {
+                byteBuffer.write(buffer, 0, len)
+            }
+            return byteBuffer.toByteArray()
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    } finally {
+        try {
+            inputStream?.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+    return null
+}
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun GenderSelector() {
-    val options = listOf("Male", "Female")
+    val options = listOf(stringResource(R.string.male), stringResource(R.string.female))
     var expanded by remember { mutableStateOf(false) }
     var selectedOptionText by remember { mutableStateOf<String?>(null) }
 
@@ -184,14 +225,12 @@ private fun TextField(
 fun TypeSelector() { //TODO
     val types = PetTypeDataUi.entries.toList()
     var selectedType by remember { mutableStateOf(types[0]) }
-
     LazyVerticalGrid(
         modifier = Modifier.padding(horizontal = 34.dp), columns = GridCells.Fixed(3)
     ) {
         items(types) { item ->
             TypeItem(
-                name = stringResource(id = item.localizedName),
-                painter = painterResource(id = item.drawableRes),
+                item = item,
                 isSelected = selectedType == item,
                 onClick = { selectedType = item })
         }
@@ -199,7 +238,7 @@ fun TypeSelector() { //TODO
 }
 
 @Composable
-fun TypeItem(name: String, painter: Painter, isSelected: Boolean = false, onClick: () -> Unit) {
+fun TypeItem(item: PetTypeDataUi, isSelected: Boolean = false, onClick: () -> Unit) {
     Card(modifier = Modifier
         .clickable { onClick() }
         .padding(vertical = 8.dp, horizontal = 32.dp)
@@ -217,12 +256,13 @@ fun TypeItem(name: String, painter: Painter, isSelected: Boolean = false, onClic
             Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Icon(
-                painter = painter, contentDescription = name,
+                painter = painterResource(id = item.drawableRes),
+                contentDescription = stringResource(id = item.localizedName),
                 Modifier
                     .size(40.dp)
                     .padding(4.dp)
             )
-            Text(text = name)
+            Text(text = stringResource(id = item.localizedName))
         }
     }
 }
@@ -239,6 +279,6 @@ fun ProfileCreationPreview() {
 @Composable
 fun TypeItemPreview() {
     PetTracksTheme {
-        TypeItem("Cat", painterResource(id = R.drawable.cat_solid), false) {}
+        TypeItem(PetTypeDataUi.Cat, false) {}
     }
 }
