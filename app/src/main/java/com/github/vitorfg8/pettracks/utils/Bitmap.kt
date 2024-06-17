c18ae2820ddc146afd55ad321ba4b010227a3ab9
Vítor <vitorfer237@gmail.com>
2024-06-17 18:25:30 -0300

    code refactor

--- app/src/main/java/com/github/vitorfg8/pettracks/utils/Bitmap.kt
+++ app/src/main/java/com/github/vitorfg8/pettracks/utils/Bitmap.kt
@@ -12,3 +12,30 @@ import androidx.compose.ui.graphics.painter.Painter
 fun Bitmap?.asPainter(fallback: Painter = ColorPainter(Color.Transparent)): Painter {
     return this?.let { BitmapPainter(it.asImageBitmap()) } ?: fallback
 }
+
+/**
+ *Resizes a bitmap, ensuring that neither its width nor height exceeds [maxDimension]
+ * while maintaining the original aspect ratio.
+ *
+ * @param bitmap The bitmap to resize.
+ * @param maxDimension The maximum allowed dimension (width or height) for the resized bitmap.
+ * @return The resized bitmap.
+ */
+fun resizeBitmap(bitmap: Bitmap, maxDimension: Int = 512): Bitmap {
+    val width = bitmap.width
+    val height = bitmap.height
+
+    if (width <= maxDimension && height <= maxDimension) {
+        return bitmap
+    }
+
+    val scaleFactor = if (width > height) {
+        maxDimension / width.toFloat()
+    } else {
+        maxDimension / height.toFloat()
+    }
+    val newWidth = (width * scaleFactor).toInt()
+    val newHeight = (height * scaleFactor).toInt()
+
+    return Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true)
+}
