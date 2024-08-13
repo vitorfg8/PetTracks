c0d58cce6aba657d88f4849147a01199634c8f94
Vítor Ferreira Guimarães <vitorfer237@gmail.com>
2024-08-13 14:03:49 -0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.github.vitorfg8.pettracks"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.github.vitorfg8.pettracks"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
